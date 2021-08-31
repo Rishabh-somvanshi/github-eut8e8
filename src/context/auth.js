208349d0a8f739da01e82614f94d66938080446e
Rishabh-somvanshi <rishabhsomvanshi87@gmail.com>
2021-08-31 21:24:50 +0530

    comments

--- src/context/auth.js
+++ src/context/auth.js
@@ -1,5 +1,7 @@
 import { createContext, useContext } from 'react';
-
+/* 
+-Context provides a way to pass data through the component tree without having to pass props down manually at every level. 
+-use for global data*/
 export const AuthContext = createContext();
 
 export function useAuth() {
